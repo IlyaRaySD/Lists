4d3f970bb5c17f66cd6158f6c76d144f0684188f
IlyaRaySD <xray2003jy@gmail.com>
2023-12-13 03:00:19 +0300

    Добавьте файлы проекта.

---
+++ Lists/List_class.h
@@ -0,0 +1,94 @@
+#pragma once
+
+template <typename Type>
+class List {
+private:
+	class Node {
+	public:
+		Type value;
+		Node* next;
+
+		Node(int value) {
+			this->value = value;
+			next = nullptr;
+		}
+	};
+
+	Node* head;
+	int length;
+public:
+	List() {
+		head = nullptr;
+		length = 0;
+	}
+
+	int len() { return length; }
+
+	Node* head() { return head; }
+
+	void append(Type value) {
+		if (head == nullptr) {
+			head = new Node(value);
+			length = 1;
+		}
+		else {
+			Node* current = head;
+			while (current->next != nullptr)
+				current = current->next;
+			current->next = new Node(value);
+			length++;
+		}
+
+	}
+
+	void insert(Type value, int index) {
+		if (index == 0) {
+			Node* temp = head;
+			head = new Node(value);
+			head->next = temp;
+			length++;
+		}
+		else {
+			Node* current = head;
+			for (int i = 0; i < index - 1; i++)
+				current = current->next;
+			Node* temp = current->next;
+			current->next = new Node(value);
+			current->next->next = temp;
+			length++;
+		}
+	}
+
+	void del(int index) {
+		Node* current = head;
+		for (int i = 0; i < index; i++)
+			current = current->next;
+		Node* temp = current->next;
+		current->next = temp->next;
+		delete temp;
+		length--;
+	}
+
+	void clear() {
+		for (int i = 0; i < length; i++) {
+			Node* temp = head;
+			head = head->next;
+			delete temp;
+		}
+		head = nullptr;
+		length = 0;
+	}
+
+	Type operator[](int index) {
+		Node* current = head;
+		for (int i = 0; i < index; i++)
+			current = current->next;
+		return current->value;
+	}
+
+	void merger(Node* node) {}
+
+	~List() {
+		clear();
+	}
+};
---
+++ Lists/Lists.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "List_class.h"
+using namespace std;
+
+int main() {
+	int n = 5;
+	List<int> list_1, list_2;
+
+	for (int i = 0; i < n; i++) {
+		list_1.append(i + 1);
+		cout << list_1[i] << ' ';
+	}
+
+	cout << endl;
+
+	for (int i = 2; i < n + 2; i++) {
+		list_2.append(i + 1);
+		cout << list_2[i - 2] << ' ';
+	}
+
+	cout << endl;
+}
